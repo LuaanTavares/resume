e643085a46e130a561d8bc8f35a148335e2d0b6c
Luan Tavares <luankriok18@gmail.com>
2023-03-14 17:20:23 -0300

    feat: main

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -13,16 +13,16 @@ export const Main = () => {
   return(
     <>
       <section class='text-gray-50 bg-no-repeat bg-cover'style={backgroundImageStyle}>
-        <div class='pt-32 flex justify-center items-center flex-col p-4 h-full w-full bg-black bg-opacity-60'>
+        <div class='pt-24 flex justify-center items-center flex-col p-4 h-full w-full bg-black bg-opacity-60'>
                   <img class="shadow-black shadow-xl p-3 rounded-full h-80 w-80" src={profile} alt="profile-image"/>
           <div class='flex flex-col items-center'>
-            <p class=' my-10 text-4xl'>Luan Tavares</p>
-            <div class='flex gap-3 justify-center my-6'>
+            <p class=' my-4 text-4xl'>Luan Tavares</p>
+            <div class='flex gap-5 justify-center my-6'>
               <a href=""><button class=' font-medium bg-cyan-600 rounded-md px-1 w-32 h-8 hover:bg-cyan-800 duration-100'>Hire me</button></a>
               <a href=""><button class=' font-medium bg-cyan-600 rounded-md px-1 w-32 h-8 hover:bg-cyan-800 duration-100'>Download CV</button></a>
             </div>
           </div>
-          <div class=' my-6 flex justify-center gap-4 w-60'>
+          <div class=' my-1 flex justify-center gap-4 rounded-full bg-black  shadow-2xl shadow-black'>
             <a class=' rounded-full' href=""><AiFillLinkedin class='text-4xl rounded-full '/></a>
             <a class=' rounded-full' href=""><AiFillGithub class='text-4xl rounded-full ' /></a>
             <a class=' rounded-full' href=""><AiOutlineInstagram class='text-4xl rounded-full '/></a>
